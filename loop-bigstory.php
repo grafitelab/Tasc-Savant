980beb8f60537c5294a51c5ec0be0d880f41c23c
matteopelosi <matteo.pelosi@me.com>
2015-09-27 19:14:13 +0200

    Tag categoria spostato in title

--- loop-bigstory.php
+++ loop-bigstory.php
@@ -1,8 +1,14 @@
 								    	<article class="article-big-story">
 								    	<div class="tasc-post">
 								    		<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
-									    		<span class="category-tag"><?php the_category(' | '); ?></span>
-									    		<div class="title"><?php the_title(); ?><div class="createdby">Creato da <?php $author = get_the_author(); echo $author; ?> </div></div>
+									    		<div class="title">
+										    	<span class="category-highlight category-<?php foreach(get_the_category() as $category) {
+														echo $css_slug;} ?> category-<?php $category = get_the_category(); 
+														echo $category[0]->slug; ?>"><?php
+														$category = get_the_category(); 
+														echo $category[0]->cat_name;?>
+												</span><br/>
+										    	<?php the_title(); ?><div class="createdby">Creato da <?php $author = get_the_author(); echo $author; ?> </div></div>
 								    			<div class="overlay"></div> 
 								    			<?php if ( has_post_thumbnail() ) { the_post_thumbnail('large'); } else { ?><img src="<?php echo get_template_directory_uri(); ?>/library/images/default.jpg" /><?php } ?>
 							    			<div class="gradient"></div>
