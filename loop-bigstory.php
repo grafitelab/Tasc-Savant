5946b19e31c1baf506b5ace8fd59258c56882856
matteopelosi <matteo.pelosi@me.com>
2015-09-27 19:02:02 +0200

    Stile tag categoria

--- loop-bigstory.php
+++ loop-bigstory.php
@@ -1,6 +1,7 @@
 								    	<article class="article-big-story">
 								    	<div class="tasc-post">
 								    		<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+									    		<span class="category-tag"><?php the_category(' | '); ?></span>
 									    		<div class="title"><?php the_title(); ?><div class="createdby">Creato da <?php $author = get_the_author(); echo $author; ?> </div></div>
 								    			<div class="overlay"></div> 
 								    			<?php if ( has_post_thumbnail() ) { the_post_thumbnail('large'); } else { ?><img src="<?php echo get_template_directory_uri(); ?>/library/images/default.jpg" /><?php } ?>
