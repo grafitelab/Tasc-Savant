54ae96af663fb35e53c312bd2e90a8d33b1ca141
matteopelosi <matteo.pelosi@me.com>
2015-09-28 20:02:05 +0200

    Formo le sezioni in sidebar

--- sidebar.php
+++ sidebar.php
@@ -10,16 +10,36 @@
 					</div>
 				</section>
 				<section id="snacks-sidebar" class="sidebar-section">
-					
+					<div class="s-section-title clearfix">
+						<div class="s-title-border"></div>
+						<div class="s-title-background">
+							<span class="s-title">Snacks</span>
+							<div class="s-title-white-triangle"></div>
+						</div>
+						<a href="#">Tutti gli snacks ></a>
+					</div>
 				</section>
 				<section id="column-sidebar" class="sidebar-section">
-					
+					<div class="s-section-title clearfix">
+						<div class="s-title-border"></div>
+						<div class="s-title-background">
+							<span class="s-title">Rubriche</span>
+							<div class="s-title-white-triangle"></div>
+						</div>
+						<a href="#">Tutte le rubriche ></a>
+					</div>
 				</section>
 				<div id="feedly-sidebar" class="sidebar-section">
 					
 				</div>
 				<section id="other-sidebar" class="sidebar-section">
-					
+					<div class="s-section-title clearfix">
+						<div class="s-title-border"></div>
+						<div class="s-title-background">
+							<span class="s-title">Altro</span>
+							<div class="s-title-white-triangle"></div>
+						</div>
+					</div>
 				</section>
 				<section id="task-rank-sidebar" class="sidebar-section">
 					
