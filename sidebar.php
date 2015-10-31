c63992d2798fd2350d98d898ad75dd7ca82cfa21
matteopelosi <matteo.pelosi@me.com>
2015-10-31 08:53:27 +0100

    aggiungo quotes

--- sidebar.php
+++ sidebar.php
@@ -94,9 +94,18 @@
 					</div>
 					<div style="width: 100%;height: 300px;border: 1px solid;box-sizing: border-box;">
 						<span class="quote">
-				
+							<?php
+								$key = 'vyfl4dws0xnebf9ka95p9e'; 
+								$language = 'it';
+								$request = file_get_contents('https://www.getquotesapp.com/api/randomquote?key='.$key.'&ln='.$language);
+								$response = json_decode($request, true);
+								
+								$body_quote = $response['quote']['body'];
+								$author_quote = $response['quote']['author'];	
+								echo $body_quote;					
+							?>
 						</span>
-						<span class="quote-author"></span>
+						<span class="quote-author"><?php $author_quote ?></span>
 						<span><a href="#">Scarica quotes dall'app store ></a></span>
 					</div>
 				</section>
