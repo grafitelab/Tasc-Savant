7a9871a7789f3fbde5e7c1a5380f58bd79431038
matteopelosi <matteo.pelosi@me.com>
2015-12-04 08:20:09 +0100

    tassonomie rubriche

--- loop-single-product.php
+++ loop-single-product.php
@@ -1,9 +1,6 @@
 <article id="post-<?php the_ID(); ?>" <?php if(is_single()) {post_class('clearfix single product');} else {post_class('clearfix snack');} ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 							<?php get_template_part( 'parts/sharebox' ); ?>
 						    <section class="post-content post-content-article clearfix" itemprop="articleBody">
-							    <div class="leftColumn content">
-									<?php the_content();?>
-							    </div>
 							    <div class="rightColumn details">
 								    <?php $productCustomMeta = get_post_meta($post->ID,'_my_meta',TRUE);?>
 								    <span class="price"><?php if ($productCustomMeta['discounted-price'] != ""){ echo "<span class='discounted-price'>".$productCustomMeta['price'].'</span> | '.$productCustomMeta['discounted-price'];} else if ($productCustomMeta['price'] != "") { echo $productCustomMeta['price']; }?></span>
@@ -12,6 +9,9 @@
 								    	<?php if ($productCustomMeta['description'] != ""){ echo $productCustomMeta['description'];}?>
 								    </span>
 								    <a class="buyButton" href="#">Acquista</a>
+							    </div>
+							    <div class="leftColumn content">
+									<?php the_content();?>
 							    </div>
 							</section> <!-- end article section -->
 
