add7aa15884e3bbb5e06b85ee1fb1d06e89cb9ec
matteopelosi <matteo.pelosi@me.com>
2015-12-05 10:51:35 +0100

    singolo video

--- loop-videosingleheaderfull.php
+++ loop-videosingleheaderfull.php
@@ -7,15 +7,24 @@
 					?>
 					<div id="super-header">
 						<div id="large-post-header" <?php if ( has_post_thumbnail() ) { ?>style="background-image:url('<?php echo $thumb; ?>');"  <?php } ?>  >
+							<h1 class="page-title"><?php
+										$lastTerms = get_the_terms($post->ID, 'video_category' );
+											if ($lastTerms && ! is_wp_error($lastTerms)) :
+												$term_slugs_arr = array();
+												foreach ($lastTerms as $term) {
+													$term_slugs_arr[] = $term->slug;
+												}
+												$terms_slug_str = join( " ", $term_slugs_arr);
+											endif;
+											echo $terms_slug_str;
+									?><div class="m-border"></div></h1>
+							<div class="featured-last" <?php if ( has_post_thumbnail() ) { ?>style="background-image:url('<?php echo $thumb; ?>');"  <?php } ?>>
+								<?php if( has_post_video() ) {the_post_video();} ?>
+							</div>
+							
 							<div id="full-post-info">
 								<div id="super-title"><h1 class="single-title" itemprop="headline"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1></div>
-								<div id="super-author">
-									<h2>
-										<span class="gravatar"><?php if (function_exists('get_avatar')) { echo get_avatar( get_the_author_email(), '120' ); } ?></span>
-										<span class="author-details">VIDEO <?php the_author_posts_link(); ?> in <?php the_category(', '); ?></span>
-									</h2>
-								</div>
 							</div>
-			    			<div class="gradient"></div>
+			    			<div class="featured-background-shade"></div>
 						</div>
 					</div>
