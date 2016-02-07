10ddd864d92c37c9203cb2a70fb88f6ffc55c265
matteopelosi <matteo.pelosi@me.com>
2016-02-07 23:06:02 +0100

    abbozzo media in home

--- footer.php
+++ footer.php
@@ -1,5 +1,86 @@
 		
 		</div> <!-- end #container -->
+		<?php if ( is_home() ) {?>
+		<div class="page-template-page-video page-template-page-shop">
+		<div id="content-top">
+		<div id="m-header">
+				<div class="home-video" style="position: relative;">
+					<?php
+						$lastCustomVideo = new WP_Query( 'post_type=video&posts_per_page=1' );
+						if ($lastCustomVideo->have_posts()) {
+							while ($lastCustomVideo->have_posts()) {
+								$lastCustomVideo->the_post();
+								
+								
+				global $post;
+				//get the right thumbnail
+				$thumb_id = get_post_thumbnail_id();
+				$thumb_url = wp_get_attachment_image_src($thumb_id,'thumb-big', true);
+				$thumb= $thumb_url[0];
+			
+    				?>
+					<div class="featured-background"<?php if ( has_post_thumbnail() ) { ?>style="background-image:url('<?php echo $thumb; ?>'); height: 700px; margin: 0;"  <?php } ?>><div class="featured-background-shade"></div></div>
+					<h1 class="page-title">Ultimo Video<div class="m-border"></div></h1>
+					<div class="featured-last" <?php if ( has_post_thumbnail() ) { ?>style="background-image:url('<?php echo $thumb; ?>');"  <?php } ?>>
+						<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>" class="divLink"></a>
+						<h2 class="last-category">
+							<?php
+								$lastTerms = get_the_terms($post->ID, 'video_category' );
+									if ($lastTerms && ! is_wp_error($lastTerms)) :
+										$term_slugs_arr = array();
+										foreach ($lastTerms as $term) {
+											$term_slugs_arr[] = $term->slug;
+										}
+										$terms_slug_str = join( " ", $term_slugs_arr);
+									endif;
+									echo $terms_slug_str;
+							?><div class="m-border"></div>
+						</h2>
+						<h1 class="last-title"><?php the_title(); ?></h1>
+						<div class="overlay"></div> 
+						<div class="gradient"></div>
+					</div>
+					<?php
+							}
+						}
+    				?>
+				</div>
+			<div id="home-shop" style="position: relative;">
+					<div class="featured-background" style="height: 700px; margin: 0;"><div class="featured-background-shade"></div></div>
+					<h1 class="page-title">Tasc Shop<div class="m-border"></div></h1>
+					<h2 class="page-subtitle">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</h2>
+					<div id="featured-product-container">
+					<?php
+						$lastCustomProduct = new WP_Query( 'post_type=product&posts_per_page=3' );
+						if ($lastCustomProduct->have_posts()) {
+							while ($lastCustomProduct->have_posts()) {
+								$lastCustomProduct->the_post();
+								
+								
+				global $post;
+				//get the right thumbnail
+				$thumb_id = get_post_thumbnail_id();
+				$thumb_url = wp_get_attachment_image_src($thumb_id,'thumb-normal', true);
+				$thumb= $thumb_url[0];
+				$productCustomMeta = get_post_meta($post->ID,'_my_meta',TRUE);
+			
+    				?>
+					<div class="featured-product" <?php if ( has_post_thumbnail() ) { ?>style="background-image:url('<?php echo $thumb; ?>'); background-size: cover;"  <?php } ?>>
+						<a class="absoluteLink" href="<?php the_permalink() ?>"></a>
+						<h1 class="product-title"><?php the_title(); ?></h1>
+						<span class="featured-price"><?php $productCustomMeta['price'] ?></span>
+					</div>
+					<?php
+							}
+						}
+    				?>
+					</div>
+			</div>
+			
+		</div>
+		</div>
+		</div>
+		<?php } ?>
 		<footer class="footer" role="contentinfo"
 			<?php
 			//seleziono immagine casuale per sfondo footer
